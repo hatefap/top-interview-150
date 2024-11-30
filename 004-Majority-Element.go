51be7b37ab238474925113d09389aa6273751cc8
Hatef Alipour <alipoorhatef1994@gmail.com>
2024-11-30 21:24:19 +0330

    Majority Element

---
+++ 004-Majority-Element.go
@@ -0,0 +1,16 @@
+package main
+
+func majorityElement(nums []int) int {
+	c, n := nums[0], 1
+	for i := 1; i < len(nums); i++ {
+		if n == 0 {
+			c, n = nums[i], 1
+			continue
+		} else if nums[i] == c {
+			n++
+		} else {
+			n--
+		}
+	}
+	return c
+}
